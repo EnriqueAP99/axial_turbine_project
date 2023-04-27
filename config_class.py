511ea3f042be338c64de8bd76fab9db933c18ae7
EnriqueAP99 <enrique.aragonpabon@alum.uca.es>
2023-04-27 23:38:32 +0200

    Retoques para aproximación precisa en asíntota vertical

--- config_class.py
+++ config_class.py
@@ -27,6 +27,7 @@ class config_parameters:
     geom: dict = None  # Diccionario para almacenar parámetros geométricos de la turbina
     iter_limit: int = 600  # Límite de iteraciones que se establece
     relative_jump: float = 0.1  # Salto relativo durante la búsqueda cuando se conoce la presión a la salida
+    accurate_approach: bool = False  # Opción recomendable cuando el salto de presión es alto.
 
     def __post_init__(self):
         if self.loss_model not in ['Aungier', 'Ainley_and_Mathieson']:
