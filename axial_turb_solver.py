e36d8262958afa17532b74395a479c6d924e2b93
EnriqueAP99 <enrique.aragonpabon@alum.uca.es>
2023-05-07 17:42:39 +0200

    Cambios para el funcionamiento del algoritmo de búsqueda.

--- axial_turb_solver.py
+++ axial_turb_solver.py
@@ -146,6 +146,14 @@ def solver_decorator(cfg: config_class, p_out: float | None, C_inx_estimated: fl
             rel_error = None
             p_out_iter = None
             f_a = f_b = None
+            diff_value = None
+
+            def get_C_inx():
+                nonlocal diff_value, C_inx
+                diff_value = ((p_out_iter_b-p_out) * (C_inx_b - C_inx_a) / (p_out_iter_b - p_out_iter_a))
+                C_inx = C_inx_b - diff_value
+                if C_inx < C_inx_a or diff_value < solver_relative_error*C_inx_a:
+                    C_inx = (C_inx_b + C_inx_a)/2
 
             while rel_error is None or rel_error >= cfg.relative_error:  # Applying Regula Falsi
                 iter_count += 1
@@ -153,10 +161,7 @@ def solver_decorator(cfg: config_class, p_out: float | None, C_inx_estimated: fl
                     rel_error = solver_relative_error
                     f_a = p_out_iter_a-p_out
                     f_b = p_out_iter_b-p_out
-                    diff_value = ((p_out_iter_b-p_out) * (C_inx_b - C_inx_a) / (p_out_iter_b - p_out_iter_a))
-                    C_inx = C_inx_b - diff_value
-                    if C_inx < C_inx_a or diff_value < solver_relative_error:
-                        C_inx = (C_inx_b + C_inx_a)/2
+                    get_C_inx()
                 try:
                     ps_list = inner_funtion_from_problem_solver(C_inx)
                 except NonConvergenceError:
@@ -183,15 +188,14 @@ def solver_decorator(cfg: config_class, p_out: float | None, C_inx_estimated: fl
                         C_inx_a = C_inx
                         p_out_iter_a = p_out_iter
                         rel_error = fabs(f_c) / p_out
-                        diff_value = ((p_out_iter_b-p_out) * (C_inx_b - C_inx_a) / (p_out_iter_b - p_out_iter_a))
-                        C_inx = C_inx_b - diff_value
+                        get_C_inx()
                     elif f_c < 0:  # f_c * f_a < 0
                         f_c = f_b = p_out_iter - p_out
                         C_inx_b = C_inx
                         p_out_iter_b = p_out_iter
                         rel_error = fabs(f_c) / p_out
-                        diff_value = ((p_out_iter_b-p_out) * (C_inx_b - C_inx_a) / (p_out_iter_b - p_out_iter_a))
-                        C_inx = C_inx_b - diff_value
+                        get_C_inx()
+
 
                 record.info('Error de presión a la salida: %.10f  ...  Valor actual: %.2f Pa ...  '
                             'Valor objetivo: %.2f Pa', rel_error, p_out_iter, p_out)
