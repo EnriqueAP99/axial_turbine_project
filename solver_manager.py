d9228697467db8877d02fd8ddfd2e0b35442084e
EnriqueAP99 <129470076+EnriqueAP99@users.noreply.github.com>
2023-08-27 06:49:36 +0200

    Update solver_manager.py

--- solver_manager.py
+++ solver_manager.py
@@ -497,7 +497,7 @@ def main():
                     x_label_name_and_units = x_label_name + units_extension
                 else:
                     dep_ids_dict[item] = [item]
-                    dep_ids_dict[item].append(item + units_extension)  # For y-labels when not multiplotting.
+                    dep_ids_dict[item].append(item + units_extension)  # For y-labels when not plotting.
                     dep_ids_dict[item].append(item)  # This one is for leyends when needed.
                     dep_ids_dict[item].append(item + units_extension)  # For y-labels when multiplotting.
             else:
